46b3a90d4473e8ca232d94fd778e02d45b0fd50b
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-05-11 15:51:37 -0400

    Initial attempt at a manifest select widget for admin views

--- djiffy/admin.py
+++ djiffy/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.forms import Select
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Manifest, Canvas
+
+
+class ManifestSelectWidget(Select):
+    '''Select widget for a :class:`~djiffy.models.Manifest` that also
+    displays thumbnail and links to view and edit the Manifest.'''
+
+    def render(self, name, value, attrs=None):
+        widget = super(ManifestSelectWidget, self).render(name, value, attrs)
+        if value is not None:
+            manifest = Manifest.objects.get(pk=value)
+            # NOTE: should really only display edit link if user has
+            # the change permission on manifests
+            # FIXME: formatting / layout issues
+            return mark_safe(
+                '<div>%s</div><br/> <div style="float:left">%s <a href="%s">view</a> | <a href="%s">edit</a></div>' % \
+                    (widget, manifest.admin_thumbnail(),
+                     manifest.get_absolute_url(),
+                     reverse('admin:djiffy_manifest_change',
+                     args=[manifest.id]))
+                )
+
+        return widget
+
 
 class CanvasInline(admin.StackedInline):
     model = Canvas
