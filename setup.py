252fcedab9619a20d2bc512fc806e03cc37cad42
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-09-28 10:19:38 -0400

    Add mysqlclient to test dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-test_requirements = ['pytest', 'pytest-django', 'pytest-cov'],
+test_requirements = ['pytest', 'pytest-django', 'pytest-cov', 'mysqlclient'],
 
 setup(
     name='djiffy',
