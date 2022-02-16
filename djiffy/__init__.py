3c2137bb02f7857bb164b5660eda5b4418bd5ae3
rlskoeser <rebecca.s.koeser@princeton.edu>
2022-02-16 14:10:53 -0500

    Set version to 0.7.1

--- djiffy/__init__.py
+++ djiffy/__init__.py
@@ -1,6 +1,6 @@
 default_app_config = 'djiffy.apps.DjiffyConfig'
 
-__version_info__ = (0, 8, 0, 'dev')
+__version_info__ = (0, 7, 1, None)
 
 
 # Dot-connect all but the last. Last is dash-connected if not None.
