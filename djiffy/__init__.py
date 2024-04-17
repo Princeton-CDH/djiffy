90b62285ab96a099bc0eb07a8b5a2dd8ba96b716
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-04-17 16:10:13 -0400

    Migrate from setup.py to pyproject.toml

--- djiffy/__init__.py
+++ djiffy/__init__.py
@@ -1,9 +1,3 @@
-default_app_config = 'djiffy.apps.DjiffyConfig'
+default_app_config = "djiffy.apps.DjiffyConfig"
 
-__version_info__ = (0, 9, 0, "dev")
-
-
-# Dot-connect all but the last. Last is dash-connected if not None.
-__version__ = '.'.join([str(i) for i in __version_info__[:-1]])
-if __version_info__[-1] is not None:
-    __version__ += ('-%s' % (__version_info__[-1],))
+__version__ = "0.9.0.dev0"
